e966c62c8d607cb71d98a78cdac1e75350be0ad7
Juan Carlos Perez Castro <juanca0423@gmail.com>
2026-01-10 06:39:30 -0600

    update: 2026-01-10 06:39

--- queries/go/textobjects.scm
+++
@@ -1,8 +0,0 @@
-;;queries/go/textobjects.scm
-(function_declaration
-  name: (identifier) @test.name
-  parameters: (parameter_list
-    (parameter_declaration
-      type: (pointer_type) @_recv))
-  (#match? @_recv "*T")
-) @test.type
